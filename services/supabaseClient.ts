9c2bf470c168e66b7d560abef8a4039421cd765f
EbbuzRM <borlo92@gmail.com>
2025-07-23 13:22:57 +0200

    fix secret exposed

--- services/supabaseClient.ts
+++ services/supabaseClient.ts
@@ -1,16 +1,19 @@
 import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from './secrets';
 
-const supabaseUrl = SUPABASE_URL;
-const supabaseAnonKey = SUPABASE_ANON_KEY;
+// Le chiavi ora vengono lette dalle variabili d'ambiente
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Supabase URL and Anon Key must be provided in .env file");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
-
     persistSession: true,
     detectSessionInUrl: false,
   },
